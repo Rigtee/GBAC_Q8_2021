29d63a4ba7aa87e0205adb4349ddc09120799f2c
tomrigal <tom.rigal@outlook.com>
2021-12-06 13:50:38 +0100

    Mid day update

--- reduce_db.py
+++ reduce_db.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
 db1 = pd.read_csv('master_data/Anagrafica_ClubQ8.csv', sep=';')
+db2 = pd.read_csv('master_data/Premi_ClubQ8.csv', sep=';')
+db3 = pd.read_csv('master_data/Rifornimenti_Carburante_ClubQ8.csv', sep=';')
 
 db1_head = db1.head(1000)
+db2_head = db2.head(1000)
+db3_head = db3.head(1000)
 
-db1_head.to_csv('copy_master_data/Anagrafica_red.csv')
-
+db1_head.to_csv('copy_master_data/Anagrafica_ClubQ8_red.csv', sep = ';')
+db2_head.to_csv('copy_master_data/Premi_ClubQ8_red.csv', sep = ';')
+db3_head.to_csv('copy_master_data/Rifornimenti_Carburante_ClubQ8_red.csv', sep = ';')
